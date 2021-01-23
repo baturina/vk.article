c352fb13b3a378ce3f52c3e160435e816fc18d9f
Anastasia Baturina <89099934219@mail.ru>
2021-01-24 01:29:22 +0300

    v2

---
+++ src/main/java/domain/Attachments.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Attachments {
+    public String getAttachments() {
+        return Attachments;
+    }
+
+    public void setAttachments(String attachments) {
+        Attachments = attachments;
+    }
+
+    private String Attachments;
+}
