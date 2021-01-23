e03f6352c48b04f092deafb458b4df6525355180
Anastasia Baturina <89099934219@mail.ru>
2021-01-24 01:31:07 +0300

    manager

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,4 +1,27 @@
 package manager;
 
+import domain.Post;
+
 public class PostManager {
+
+
+    private Post[] objects;
+
+    public void Post(Post []objects) {
+        this.objects = objects;
+    }
+
+    public Post search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+
+        return null;
+    }
+
+    public Post[] getObjects() {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int post_id) {
+
+    }
 }
